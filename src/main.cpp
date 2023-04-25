12f2f239addfd5f4a1247cd02ca6f3592b7535ef
reto ambrosini <reto.ambrosini@gmail.com>
2023-04-25 16:01:23 +0200

    Versione funzionante.
    Il potenzionetro P2 controlla il led sulla porta 13 tramite pwm

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <Arduino.h>
 
+#define VCC 4.972	// tensione di alimentazione
+
 // potenziometri
 #define POT_1 A6
 #define POT_2 A7
@@ -8,7 +10,11 @@
 #define S_1 62
 #define S_2 63
 
+//LED
+#define LED_PWM 13
+
 uint32_t tempo=0;
+float adc_conv=VCC/1023;
 
 void setup()
 {
@@ -17,16 +23,28 @@ void setup()
 
 void loop() 
 {
-	uint8_t tasto_1;
-	tasto_1=digitalRead(S_1);
+	uint8_t tasto_1=digitalRead(S_1);
+	uint16_t valore_adc = analogRead(POT_2);
+	uint8_t valore_pwm = valore_adc>>2;
+	analogWrite(LED_PWM,valore_pwm);
 
 	if (tasto_1==0)
 	{
 		if (millis() > (tempo + 500))
 		{
-			uint16_t valore_adc = analogRead(POT_2);
+			PORTD=0x01;
+			float tensione = valore_adc*adc_conv;
+			PORTD=0x00;
 			Serial.print("valore adc = ");
 			Serial.print(valore_adc);
+			Serial.write(0x0a);
+			Serial.print("tensione = ");
+			Serial.print(tensione,3);
+			Serial.print(" V");
+			Serial.write(0x0a);
+			Serial.print("valore pwm = ");
+			Serial.print(valore_pwm);
+			Serial.write(0x0a);
 			Serial.write(0x0a);
 			tempo=millis();
 		}
